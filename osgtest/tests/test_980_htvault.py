963bc1a411465286bf6def42fdb869d6c9a53e08
Matthew Westphall <westphall@wisc.edu>
2025-07-24 16:36:25 -0500

    attempt to configure htvault

---
+++ osgtest/tests/test_980_htvault.py
@@ -0,0 +1,53 @@
+import re
+from os.path import join
+
+import osgtest.library.core as core
+import osgtest.library.files as files
+import osgtest.library.condor as condor
+import osgtest.library.osgunittest as osgunittest
+import osgtest.library.service as service
+
+HTVAULT_ISSUER_CONFIG = '''
+issuers:
+  - name: cilogon
+    clientid: xxx
+    url: https://cilogon.org
+    callbackmode: direct
+    credclaim: wlcg.credkey
+    roles:
+      - name: default
+        scopes: [profile,email,org.cilogon.userinfo,storage.read:,storage.create:]
+      - name: readonly
+        scopes: [profile,email,org.cilogon.userinfo,storage.read:]
+'''
+
+HTVAULT_SECRET_CONFIG = '''
+issuers:
+  - name: cilogon
+    secret: xxx
+'''
+
+class TestStartHTVault(osgunittest.OSGTestCase):
+
+    def test_01_start_htvault(self):
+        core.state['vault.started-service'] = False
+        core.state['vault.running-service'] = False
+
+        core.skip_ok_unless_installed('htvault', 'htgettoken')
+
+        core.config['vault.config-dir'] = '/etc/htvault-config/config.d/'
+
+        if service.is_running('vault'):
+            core.state['vault.running-service'] = True
+            return
+
+        core.config['vault.issuer-config'] = join(core.config['vault.config-dir'], '20-cilogon.yaml')
+        files.write(core.config['vault.issuer-config'], HTVAULT_ISSUER_CONFIG, owner='vault', chmod=0o644)
+
+        core.config['vault.secrets-config'] = join(core.config['vault.config-dir'], '80-secrets.yaml')
+        files.write(core.config['vault.secrets-config'], HTVAULT_SECRET_CONFIG, owner='vault', chmod=0o644)
+
+        service.check_start('vault')
+        core.state['vault.started-service'] = True
+        core.state['vault.running-service'] = True
+
