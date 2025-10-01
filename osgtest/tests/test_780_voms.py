22453ff593e5e9aaad2d37aa3e326201be78fcc9
Matyas Selmeci <mselmeci@wisc.edu>
2025-10-01 18:32:07 -0500

    Drop voms-server tests

--- osgtest/tests/test_780_voms.py
+++ osgtest/tests/test_780_voms.py
@@ -1,6 +1,5 @@
 import osgtest.library.core as core
 import osgtest.library.files as files
-import osgtest.library.service as service
 import osgtest.library.osgunittest as osgunittest
 import osgtest.library.voms as voms
 
@@ -9,32 +8,8 @@ class TestStopVOMS(osgunittest.OSGTestCase):
 
     # ==========================================================================
 
-    def test_01_stop_voms(self):
-        voms.skip_ok_unless_server_is_installed()
-        self.skip_ok_unless(core.state['voms.started-server'], 'did not start server')
-
-        service.check_stop(core.config['voms_service'])
-
     def test_02_restore_vomses(self):
         voms.skip_ok_unless_can_make_proxy()
 
         voms.destroy_lsc(core.config['voms.vo'])
         files.restore('/etc/vomses', 'voms')
-
-
-    def test_03_remove_vo(self):
-        voms.skip_ok_unless_server_is_installed()
-
-        # Really remove database -- the voms-admin-configure command above does
-        # not actually destroy the mysql database.
-        voms.destroy_db(core.config['voms.vo'], core.config['voms.dbusername'])
-        voms.destroy_voms_conf(core.config['voms.vo'])
-
-
-    def test_04_remove_certs(self):
-        voms.skip_ok_unless_server_is_installed()
-        core.state['voms.removed-certs'] = False
-        # Do the keys first, so that the directories will be empty for the certs.
-        core.remove_cert('certs.vomskey')
-        core.remove_cert('certs.vomscert')
-        core.state['voms.removed-certs'] = True
